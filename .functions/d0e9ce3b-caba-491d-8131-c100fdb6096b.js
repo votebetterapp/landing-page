33a113de172855526c6c3d76a8de9d4dcf6bfd8a
Matt Welke <matt.welke@groupbyinc.com>
2019-04-02 22:09:56 -0400

    wip

--- .functions/d0e9ce3b-caba-491d-8131-c100fdb6096b.js
+++ .functions/d0e9ce3b-caba-491d-8131-c100fdb6096b.js
@@ -1,5 +1,4 @@
 exports.handler = function(event, context, callback) {
     // your server-side functionality
-    console.info(`event :${event}`);
-    console.info(`cid: ${event.body.cid}`);
+    console.info(`event body: ${event.body}`);
 }
