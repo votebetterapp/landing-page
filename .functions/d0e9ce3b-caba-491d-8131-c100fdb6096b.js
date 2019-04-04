63965bf653cc5169413316c09fa73f700ad385e3
Matt Welke <mattwelke@gmail.com>
2019-04-03 22:36:13 -0400

    reduced logging

--- .functions/d0e9ce3b-caba-491d-8131-c100fdb6096b.js
+++ .functions/d0e9ce3b-caba-491d-8131-c100fdb6096b.js
@@ -6,29 +6,29 @@ exports.handler = function (event, context, callback) {
 
     // Get IP from event included header
     const uip = event.headers['x-forwarded-for'];
-    console.info(`User IP from event headers: ${uip}`);
+    // console.info(`User IP from event headers: ${uip}`);
 
     // Get other properties important for GA tracking from event body
     const cid = body.cid;
-    console.info(`Client ID from event body: ${cid}`);
+    // console.info(`Client ID from event body: ${cid}`);
 
     const dl = body.dl;
-    console.info(`Document location URL from body: ${dl}`);
+    // console.info(`Document location URL from body: ${dl}`);
 
     const dr = body.dr;
-    console.info(`Document referrer from body: ${dr}`);
+    // console.info(`Document referrer from body: ${dr}`);
 
     const ua = body.ua;
-    console.info(`User Agent from body: ${ua}`);
+    // console.info(`User Agent from body: ${ua}`);
 
     const sr = body.sr;
-    console.info(`Screen Resolution from body: ${sr}`);
+    // console.info(`Screen Resolution from body: ${sr}`);
 
     const vp = body.vp;
-    console.info(`Viewport size from body: ${vp}`);
+    // console.info(`Viewport size from body: ${vp}`);
 
     const ul = body.ul;
-    console.info(`User Language from body: ${ul}`);
+    // console.info(`User Language from body: ${ul}`);
 
     const gaPayload = querystring.stringify({
         v: '1',
@@ -58,28 +58,13 @@ exports.handler = function (event, context, callback) {
 
     const req = https.request(options, res => {
         console.info(`GA POST response status code: ${res.statusCode}`);
-
-        res.on('data', d => {
-            console.info(`GA POST successful.`);
-        });
     });
 
     req.on('error', e => {
+        console.error(`GA POST error: ${e.message}`);
         throw e;
     })
 
     req.write(gaPayload);
     req.end();
-
-    // axios.post(
-    //     'https://www.google-analytics.com/collect',
-    //     gaPayload,
-    //     {
-    //         headers: {
-    //             'Content-Type': 'application/x-www-form-urlencoded'
-    //         },
-    //     },
-    // ).then(() => {
-    //     console.info('Axios post to GA successful.');
-    // });
 }
